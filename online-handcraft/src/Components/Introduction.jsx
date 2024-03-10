523e018c1ed9930478f6b10541b95153fa6bf8a4
Gayathri <gayathri.k2022cse@sece.ac.in>
2024-03-10 22:12:04 +0530

    Intro

---
+++ online-handcraft/src/Components/Introduction.jsx
@@ -0,0 +1,25 @@
+
+const Introduction=()=>{
+    return(
+        <>
+<section className="info">
+<div>
+<div className="infodiv">
+    <nav>
+        <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Our Story</a></li>
+        <li><a href="#">Shop</a></li>
+        <li><a href="#">Wholesale</a></li>
+        <li><a href="#">Contact</a></li>
+    </ul>
+    </nav>
+</div>
+</div>
+
+</section>
+</>
+)
+    }
+
+export default Introduction;
