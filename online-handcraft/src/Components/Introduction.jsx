3e0880d3560529b013f5767feb4f6b3f0388a482
charubala.b2022cse@sece.ac.in <charubala.b2022cse@sece.ac.in>
2024-03-11 22:04:04 +0530

    add-popup

--- online-handcraft/src/Components/Introduction.jsx
+++ online-handcraft/src/Components/Introduction.jsx
@@ -1,32 +1,34 @@
 import { useState } from 'react'
+import Popup from './Popup';
 
 const Introduction=()=>{
+    const [isOpen, setIsOpen] = useState(false);
 
+    const togglePopup = () => {
+      setIsOpen(!isOpen);
+      console.log("Sucess")
+    }
+  
     
     return(
         <>
-
 <div>
 <div className="infodiv">
     <nav>
         <ul>
         <li><a href="#">Home</a></li>
         <li><a href="#">Our Story</a></li>
-        <li><a href="#">Shop</a></li>
+        <li><a href="#" onClick={togglePopup}>Shop</a></li>
+        {isOpen && <Popup
+        handleClose={togglePopup}
+        />}
+
         <li><a href="#">Wholesale</a></li>
         <li><a href="#">Contact</a></li>
     </ul>
     </nav>
 </div>
 </div>
-
-
-
-
-
-
-
-
 </>
 )
     }
