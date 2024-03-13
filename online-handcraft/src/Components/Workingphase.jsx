8e8b4d8b90768f2dff9fea28d9a8e0f46502c609
Gayathri <gayathri.k2022cse@sece.ac.in>
2024-03-13 11:46:04 +0530

    image

--- online-handcraft/src/Components/Workingphase.jsx
+++ online-handcraft/src/Components/Workingphase.jsx
@@ -3,7 +3,7 @@ const Workingphase=()=>{
     <>
     <section className="work">
         <div className="work1">
-         <img src="src\Components\workphase.png" className="imgwp"></img>
+         <img src="src\Components\workphase1.png" className="imgwp"></img>
         </div>
         <div className="work2">
         <img src="src\Components\workphase.png" className="imgwp"></img>
